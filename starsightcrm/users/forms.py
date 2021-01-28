297724755a45a49e70c5ce8389f58683f234ef7c
Sheriff-AO <sheriff.afolabi35@gmail.com>
2021-01-28 07:55:14 +0100

    adjusted checklist

--- starsightcrm/users/forms.py
+++ starsightcrm/users/forms.py
@@ -12,11 +12,23 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class ProfileUpdateForm(forms.ModelForm):
